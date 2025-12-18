f3d311b3d293eafaad471fdfa1f82bdbc20dad22
quangduy2301 <quangduy.dang23012009@gmail.com>
2025-12-18 17:52:40 +0700

    Initial commit

---
+++ myblog/about/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+
+def index(rq):
+    return HttpResponse(loader.get_template('index.html').render())
+
+def index2(rq):
+    return HttpResponse(loader.get_template('index.html').render())
