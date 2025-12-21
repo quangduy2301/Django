78fdedb42839b20955b578cf883668df561f2695
quangduy2301 <quangduy.dang23012009@gmail.com>
2025-12-21 17:57:03 +0700

    Thêm: Trang liên hệ, thay đổi giao diện trang.

--- myblog/contact/views.py
+++ myblog/contact/views.py
@@ -1,3 +1,10 @@
+# Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+
+def index(rq):
+    return HttpResponse(loader.get_template('index.html').render())
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -21,5 +21,7 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('about', include('about.urls'))
+    path('home/', include('home.urls')),
+    path('about/', include('about.urls')),
+    path('contact/', include('contact.urls')),
 ]
