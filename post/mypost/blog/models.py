7c69e868e90a4b34fe6150ca3d10fcb3d4926529
quangduy2301 <quangduy.dang23012009@gmail.com>
2026-03-08 20:22:29 +0700

    Thêm: Form đăng nhập.

--- post/mypost/blog/models.py
+++ post/mypost/blog/models.py
@@ -8,13 +8,13 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.title
     
-    def get_absolute_url(self):
-        return reverse(viewname="post-detail", kwargs={"pk": self.pk})
+    # def get_absolute_url(self):
+    #     return reverse(viewname="post-detail", kwargs={"pk": self.pk})
     
 # class Comments(models.Model):
 #     post = models.ForeignKey(Post, related_name="comments")
