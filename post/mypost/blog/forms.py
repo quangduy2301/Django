fc0c1e5e3b29c3f64d3fa84453f5ef07dbeb6602
quangduy2301 <quangduy.dang23012009@gmail.com>
2026-03-15 16:25:10 +0700

    .

---
+++ post/mypost/blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fiends = ['username','password']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'username'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Password'
+            })
+        }
