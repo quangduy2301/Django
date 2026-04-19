8be0e5d93c449ca448bd36e32f22521a8abadc2c
quangduy2301 <quangduy.dang23012009@gmail.com>
2026-04-19 16:20:03 +0700

    Changes Something

--- post/mypost/blog/models.py
+++ post/mypost/blog/models.py
@@ -15,3 +15,7 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse(viewname="post-detail", kwargs={"pk": self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models
