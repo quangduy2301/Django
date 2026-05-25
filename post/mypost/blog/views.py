088a1ecaca8ea75f7d7917b1107540ccb1655b94
quangduy2301 <quangduy.dang23012009@gmail.com>
2026-05-25 08:12:17 +0700

    Fix

--- post/mypost/blog/views.py
+++ post/mypost/blog/views.py
@@ -1,5 +1,6 @@
 from pyexpat.errors import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -104,23 +105,40 @@ def like_post(request, pk):
 
 @login_required
 def profile_view(request):
-    profile, created = Profile.objects.get_or_create(user=request.user)
-    
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
-        
-        if u_form.is_valid and p_form.is_valid():
+        u_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+        )
+
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, 'Tài khoản của bạn đã được cập nhật thành công!')
+            messages.success(
+                request,
+                'Tài khoản của bạn đã được cập nhật thành công!'
+            )
             return redirect('profile')
+
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=profile)
-            
-        context = {
-            'u_form': u_form,
-            'p_form': p_form
-        }
-        return render(request, 'profile.html', context)
+        p_form = ProfileUpdateForm(
+            instance=request.user.profile
+        )
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(
+        request,
+        'blog/profile.html',
+        context
+    )
